7bac0296d22e6f6b4cbe21c5d20ee2182670230b
xdJidx <bernardjerome59@gmail.com>
2024-04-02 14:59:38 +0200

    MongoDB utils files

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+// utils/db.js
+
+// Import MongoDB client
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+    constructor() {
+        // MongoDB connection parameters
+        this.host = process.env.DB_HOST || 'localhost';
+        this.port = process.env.DB_PORT || 27017;
+        this.database = process.env.DB_DATABASE || 'files_manager';
+        this.url = `mongodb://${this.host}:${this.port}`;
+        this.client = new MongoClient(this.url, { useUnifiedTopology: true });
+        this.client.connect();
+        this.db = this.client.db(this.database);
+    }
+
+    async isAlive() {
+        // Check if MongoDB client is connected
+        return !!this.client && !!this.client.isConnected();
+    }
+
+    async nbUsers() {
+        // Count number of documents in 'users' collection
+        return this.db.collection('users').countDocuments();
+    }
+
+    async nbFiles() {
+        // Count number of documents in 'files' collection
+        return this.db.collection('files').countDocuments();
+    }
+}
+
+// Create and export an instance of DBClient
+const dbClient = new DBClient();
+export default dbClient;
