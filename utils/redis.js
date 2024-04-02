be519dfe05ff13fe08400cbce82985594dde9713
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-04-02 12:09:59 +0200

    Redis utils  - Creating client

--- utils/redis.js
+++ utils/redis.js
@@ -1,3 +1,43 @@
-const { RedisClient } = require('redis');
+const redis = require('redis');
 
-RedisClient();
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => console.error(`Redis client error: ${error}`));
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) reject(err);
+        else resolve(reply);
+      });
+    });
+  }
+
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err, reply) => {
+        if (err) reject(err);
+        else resolve(reply);
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, reply) => {
+        if (err) reject(err);
+        else resolve(reply);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
