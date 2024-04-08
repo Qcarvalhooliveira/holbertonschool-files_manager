08e33d1d325395a009b693ab76b52002f1cf7e6f
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2024-04-08 14:20:54 +0200

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -7,13 +7,18 @@ class AuthController {
   static async getConnect(req, res) {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader || !authHeader.startsWith('Basic')) {
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
     const base64Credentials = authHeader.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [email, password] = credentials.split(':');
+
+    if (!email || !password) {
+      return res.status(401).json({ error: 'Invalid Authorization format' });
+    }
+
     const hashedPassword = sha1(password);
 
     const user = await dbClient.getUser({ email, password: hashedPassword });
